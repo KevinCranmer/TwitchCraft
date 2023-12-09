a6877eb46c85a5e41f96d06621bf552498b9ec49
KevinCranmer <ktcranme@asu.edu>
2023-12-08 19:41:37 -0600

    Initial

---
+++ src/main/java/me/crazycranberry/streamcraft/StreamCraft.java
@@ -0,0 +1,17 @@
+package me.crazycranberry.streamcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StreamCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
